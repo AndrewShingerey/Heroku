96b56d9413ce2867a43c509e8ec31db9732fb1ac
Andrew Ognew <han2332andrew@gmail.com>
2021-11-26 16:59:37 +0300

    Init commit

---
+++ src/main/java/com/example/testheroku/TestHerokuApplication.java
@@ -0,0 +1,11 @@
+package com.example.testheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHerokuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestHerokuApplication.class, args);
+    }
+}
